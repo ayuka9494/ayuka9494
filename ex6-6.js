3830da0673077ea666ed477d710270c155cbfe9c
ayuka9494 <167833160+ayuka9494@users.noreply.github.com>
2024-10-22 10:36:02 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -3,8 +3,8 @@ function sayhello66(){
 alert('Hello,'+who66+'!');
 }
 function taro66(){
-who66='太郎が来た';
+alert('太郎がきた!');}{who66='太郎';
 }
 function hanako66(){
-who66='花子が来た';
+alert('花子が来た!');}{who66='花子';
 }
