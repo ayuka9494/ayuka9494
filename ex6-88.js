b7d49e3198a044b60139675eb6cb390faa69d2b1
ayuka9494 <167833160+ayuka9494@users.noreply.github.com>
2024-11-20 20:58:54 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,1 @@
-function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/hana2.png" alt="オン">';}
+function gazo688(){document.getElementById('who688').innerHTML = '<img src="./image/hana2.png" alt="オン">';}
