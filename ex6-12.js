93e802b53eb52d9c8f8dd6c96ac43096d1e88e26
ayuka9494 <167833160+ayuka9494@users.noreply.github.com>
2024-11-26 10:38:31 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,1 +1,15 @@
-
+function hyoji(){
+//表示する文字
+var str="一文字ずつ表示します。";
+//テキストボックスの文字数
+var cnt=document.timer.maji.value.length;
+//文字が全部表示されているか確認
+if(cnt<11){
+//現在より１文字多く切り出して表示
+document.timer.maji.value=substr(0,cnt+1);}
+else{
+//全て表示されたら、空文字に戻す
+document.timer.maji.value="";}}
+function startfnc(){
+//関数hyouji()を1000ミリ秒間隔で呼び出す
+setlnterval("hyoji()",1000);}
