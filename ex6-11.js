0227cbdecfa84f129796f029bd7ee0abe646bfad
ayuka9494 <167833160+ayuka9494@users.noreply.github.com>
2024-11-28 22:02:45 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,2 +1,7 @@
-function imgchange3(){document.getElmentByld('logo').src='./image/jousaiirasuto.jpg';}
-function imgchange4(){document.getElmentByld('logo').src='./image/josai.jpg';}
+function imgchange3() {
+    document.getElementById('logo').src = './image/jousaiirasuto.jpg';
+}
+function imgchange4() {
+    document.getElementById('logo').src = './image/josai.jpg';
+}
+
