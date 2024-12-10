0d7884aede39b4460b43db9580633610f9b02633
ayuka9494 <167833160+ayuka9494@users.noreply.github.com>
2024-12-10 10:46:47 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
-function imgchange5(){document.getElementByld('flower').src='./image/flower2.png';}
-function mes2(){document.getElementByld('flower').src='./image/flower.png';}
+function imgchange5(){document.getElementById('flower').src='./image/flower2.png';}
+function mes2(){document.getElementById('flower').src='./image/flower.png';}
